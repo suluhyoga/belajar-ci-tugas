0b7f97d83d8160b6d8b337ae6975b4c118af3bdb
suluhyoga <yp7757786@gmail.com>
2025-05-29 22:01:31 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel;
+
+class Home extends BaseController
+{
+    protected $product;
+
+    function __construct()
+    {
+        helper('form');
+        helper('number');
+        $this->product = new ProductModel();
+    }
+
+    public function index()
+    {
+        $product = $this->product->findAll();
+        $data['product'] = $product;
+
+        return view('v_home', $data);
+    }
+
+    public function produk(): string
+    {
+        return view('v_produk');
+    }
+}
